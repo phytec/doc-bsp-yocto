37840837f0dfc08f2bd1a142e963fca7b3d6c7f4
Jonas Remmert <j.remmert@phytec.de>
2022-12-30 17:39:16 +0100

    gh actions: Check for filesize and invalid names
    This compliance check introduces a check on the filesize and the
    filenames.
    
    Keep image sizes small (<300 kb) to not bloat the repository. For bigger
    images we should save the files on a seperate server.
    
    Do not allow whitespaces in filenames.
    
    Signed-off-by: Jonas Remmert <j.remmert@phytec.de>

---
+++ scripts/check_filenames.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Check for whitespaces in filesnames. Exit with error, if found.
+
+DIR=source
+
+files_with_whitespace=$(find $DIR -type f -name "* *" -exec du -h {} \;)
+
+if [ -n "$files_with_whitespace" ]; then
+  echo "Error: The following files have a whitespace in the name:"
+  echo "$files_with_whitespace"
+  exit 1
+fi
