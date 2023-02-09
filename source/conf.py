16f6fde5ed9bdfc51fd1cdde357881e835fac4f7
Jonas Remmert <j.remmert@phytec.de>
2023-02-09 23:14:57 +0100

    conf: update copyright to 2023
    Signed-off-by: Jonas Remmert <j.remmert@phytec.de>

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,7 @@ import sys
 # -- Project information -----------------------------------------------------
 
 project = 'PHYTEC BSP Documentation'
-copyright = '2022, PHYTEC Messtechnik GmbH'
+copyright = '2023, PHYTEC Messtechnik GmbH'
 author = 'PHYTEC'
 
 # The full version, including alpha/beta/rc tags
