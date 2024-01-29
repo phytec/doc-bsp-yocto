a696e7ab08d16cc35c8646b8c8616916fc8397f0
Yannic Moog <y.moog@phytec.de>
2024-01-29 12:57:05 +0100

    bsp: imx8mp: add mainline-head manual
    Create a mainline-head BSP manual for i.MX 8M Plus. mainline-head is a
    non-vendor base and thus has a reduced feature-set. Some of the chapters
    found in previous release BSP manuals are omitted.
    
    Signed-off-by: Yannic Moog <y.moog@phytec.de>

--- source/conf.py
+++ source/conf.py
@@ -105,6 +105,8 @@ latex_documents = [
         'manual',
         False,
     ),
+    ('bsp/imx8/imx8mp/mainline-head', 'imx8mp-mainline-head.tex', 'i.MX 8M Plus BSP Manual',
+        'PHYTEC Messtechnik GmbH', 'manual', False,),
     ('bsp/imx8/imx8mp/pd23.1.0', 'imx8mp-pd23.1.0.tex', 'i.MX 8M Plus BSP Manual',
         'PHYTEC Messtechnik GmbH', 'manual', False,),
     ('bsp/imx8/imx8mp/pd22.1.1', 'imx8mp-pd22.1.1.tex', 'i.MX 8M Plus BSP Manual',
