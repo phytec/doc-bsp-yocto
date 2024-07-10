1c5977f421e552faee8e57dcf62f6e3385135f15
jonas-rem <jonas-rem@users.noreply.github.com>
2024-07-10 09:45:16 +0000

    deploy: b85e4514f0aed8598b6dca2c4787b46d8d84cfc0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'imx8mp-pd24.1.2-1-g96365e0',
+    VERSION: 'imx8mp-pd24.1.2-4-gb85e451',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
