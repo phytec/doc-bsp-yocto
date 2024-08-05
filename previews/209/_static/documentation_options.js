e3cb611c11eb60a2d49f2a455bb99f327d499d74
ymoog <ymoog@users.noreply.github.com>
2024-08-05 05:49:53 +0000

    deploy: 1c66f0fe9d1957fcc3b96aa7348ad435641762a3

---
+++ previews/209/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'imx8mp-pd24.1.2-23-g1c66f0f',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
