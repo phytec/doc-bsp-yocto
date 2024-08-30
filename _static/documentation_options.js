43d746a9a2fab4ab4e06d8e60721835c1b4ee11b
mschwan-phytec <mschwan-phytec@users.noreply.github.com>
2024-08-30 06:47:34 +0000

    deploy: 7f42f64b65affb9508a7988637dc2d750725fa9a

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'imx8mp-pd22.1.2-1-g9c6d218',
+    VERSION: 'imx8mp-pd22.1.2-2-g7f42f64',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
