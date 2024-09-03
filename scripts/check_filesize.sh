7e2da417feb8e9478516ce22153a5802418ef86e
Jonas Remmert <j.remmert@phytec.de>
2024-09-03 20:05:09 +0200

    gh actions: add internationalization to build
    - Update relevant github actions to build and deploy the documentation
      for all languages. The 3 relevant commands are:
      - tox -e py3-intl # Re-generate translation messages and build html
        for non-default languages.
      - tox -e py3-html # Build html documentation for the default language
        (en).
      - tox -e py3-pdf  # Build pdf documentation for all languages
    - Add a rule to ignore the filesize of .po files. They may be larger
      than the max allowed size for photos.
    
    Signed-off-by: Jonas Remmert <j.remmert@phytec.de>

--- scripts/check_filesize.sh
+++ scripts/check_filesize.sh
@@ -4,7 +4,8 @@
 MAX_SIZE=300k
 DIR=source
 
-large_files=$(find $DIR -type f -size +"$MAX_SIZE" -exec du -h {} \;)
+# Allow *.po files to be larger than the maximum size.
+large_files=$(find $DIR -type f ! -name "*.po" -size +"$MAX_SIZE" -exec du -h {} \;)
 
 if [ -n "$large_files" ]; then
   echo -e "Error: Large files were found: \n$large_files"
