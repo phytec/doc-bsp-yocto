77eba75d28368bd1a751b8dc5724690b353deba3
annie-pixel <annie-pixel@users.noreply.github.com>
2024-09-19 10:04:04 +0000

    deploy: 4e794270f6077700fa0b8e2497f989e25da85eab

---
+++ previews/219/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'imx8mp-pd22.1.2-17-g4e79427',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
