496d35a13805f5931deb7049e1f0ee2b57a286e7
jonas-rem <jonas-rem@users.noreply.github.com>
2024-09-23 09:32:06 +0000

    deploy: 2244d6a5be0e9b0e0d0ddacc832d173872ea8b2e

---
+++ previews/215/zh_CN/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'imx8mp-pd22.1.2-19-g2244d6a',
+    LANGUAGE: 'zh-CN',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
