bb563c76fc19a06f812952ec1f03cdfe4a42547b
tremmet <tremmet@users.noreply.github.com>
2024-10-09 09:07:03 +0000

    deploy: 841da05233a8c36aecabf09a8fd22a81e86c4335

---
+++ previews/226/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'imx8mp-pd22.1.2-21-g841da05',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
