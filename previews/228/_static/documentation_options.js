9fa89fc6fd1662c14572adca1a22b2f4d7c0ba47
pfiser <pfiser@users.noreply.github.com>
2024-10-14 13:52:38 +0000

    deploy: 81f90e2ac777af4f5305c0b8eb221cd5d0e1fb12

---
+++ previews/228/_static/documentation_options.js
@@ -0,0 +1,13 @@
+const DOCUMENTATION_OPTIONS = {
+    VERSION: 'imx8mp-pd22.1.2-39-g81f90e2',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
