373823d39fbf4a23df561c6b3305232ef7c1df60
BHahn42618 <BHahn42618@users.noreply.github.com>
2024-10-21 13:25:01 +0000

    deploy: 875d4b3c70b91c924fbb96749772ce3464cb5a57

--- previews/234/_static/documentation_options.js
+++ previews/234/_static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'imx8mp-pd22.1.2-35-g0dae2144',
+    VERSION: 'imx8mp-pd22.1.2-36-g875d4b3c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
