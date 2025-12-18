3d63bac097d82f1cf4111a75a5faa0e8c972f80e
Martin Schwan <m.schwan@phytec.de>
2025-12-18 13:58:33 +0100

    scripts: check_whitespace_chars: Do not use ripgrep and simplify
    Use GNU grep instead of ripgrep and simplify the script.
    
    Signed-off-by: Martin Schwan <m.schwan@phytec.de>

--- scripts/check_whitespaces_chars.sh
+++ scripts/check_whitespaces_chars.sh
@@ -1,28 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
-dir="source"
+# match NBSP (\u00a0), apostrophe (\u2019) and trailing spaces
+matches=$(grep -nrH --include "*.rsti" --include "*.rst" -P "(\x{00a0}|\x{2019}|[[:space:]]$)" source/)
 
-# Patterns:
-# 1. Trailing spaces or tabs at the end of a line.
-space_tab="[ \t]+$"
-# 2. Non-breaking space (U+00A0) anywhere.
-#pt_nbsp="\xC2\xA0"
-nbsp="[\u00A0]"
-# 3. U+2019 apostrophe anywhere.
-apostrophe="[\u2019]"
-
-# Combine patterns with OR (|) for final pattern
-pattern="($space_tab|$nbsp|$apostrophe)"
-
-# Use rg to search in .rst and .rsti files
-matches=$(rg --line-number --column "$pattern" -g '*.rst' -g '*.rsti' "$dir")
-
-# Check results
 if [[ ! -z "$matches" ]]; then
+    echo "ERROR: Detected undesired characters!"
     echo "$matches"
-    echo "Detected undesired characters or patterns."
     exit 1
-else
-    echo "No undesired characters or patterns found."
-    exit 0
 fi
