87a8ecd086736295315e41211d3a645c6ddf0fa8
Martin Schwan <m.schwan@phytec.de>
2026-01-16 13:04:42 +0100

    scripts: check_whitespace_chars: Detect more bad characters
    Detect more bad characters, such as:
    
    - trailing spaces
    - multiple, subsequent empty lines
    - missing EOL characters at end of file
    
    Signed-off-by: Martin Schwan <m.schwan@phytec.de>

--- scripts/check_whitespaces_chars.sh
+++ scripts/check_whitespaces_chars.sh
@@ -1,10 +1,37 @@
 #!/bin/sh
 
-# match NBSP (\u00a0), apostrophe (\u2019) and trailing spaces
-matches=$(grep -nrH --include "*.rsti" --include "*.rst" -P "(\x{00a0}|\x{2019}|[[:space:]]$)" source/)
+exitcode=0
 
-if [[ ! -z "$matches" ]]; then
+# match NBSP (\u00a0), apostrophe (\u2019)
+matches=$(grep --include "*.rst*" -rnP "(\x{00a0}|\x{2019})" source)
+if [ -n "$matches" ]; then
     echo "ERROR: Detected undesired characters!"
     echo "$matches"
-    exit 1
+    exitcode=1
 fi
+
+# match trailing spaces
+matches=$(grep --include "*.rst*" -rnP "[[:space:]]$" source)
+if [ -n "$matches" ]; then
+    echo "ERROR: Detected trailing spaces!"
+    echo "$matches"
+    exitcode=1
+fi
+
+# match multiple empty lines
+matches=$(grep --include "*.rst*" -rlzP "\n\n\n" source)
+if [ -n "$matches" ]; then
+    echo "ERROR: Detected redundant empty lines!"
+    echo "$matches"
+    exitcode=1
+fi
+
+# find missing EOL characters at end of file
+matches=$(grep --include "*.rst*" -rvlzP "\x{000a}$" source)
+if [ -n "$matches" ]; then
+    echo "ERROR: No newline at end of file!"
+    echo "$matches"
+    exitcode=1
+fi
+
+exit $exitcode
